b44b5f1cfc68bac1a20ce830ea55455642facd3f
Brian Enigma <brian@netninja.com>
2020-07-14 10:36:03 -0700

    Update for new directory structure.

--- seed.sh
+++ seed.sh
@@ -13,7 +13,7 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
-FOLDER="$1/$2"
+FOLDER="archives/$1/$2"
 mkdir -p "$FOLDER"
 cp sample_month.xml "$FOLDER/month.xml"
 xmlstarlet ed --pf  -u /PPMonth/year --value "$1" "$FOLDER/month.xml" > "$FOLDER/month2.xml"
